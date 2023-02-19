a62c2258e053650700682a6943b88cfae5c7974f
AntonYaroshevskii <120041153+AntonYaroshevskii@users.noreply.github.com>
2023-02-19 19:20:35 +0300

    Завершение задачи

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿string[] array1 = { "hello", "world", "2", "(.v.)", "12", "\0" };
 string[] array2 = new string[array1.Length];
 
-
+SearchArray(array1, array2);
+PrintArray(array2);
 //----------методы-------------
 void SearchArray(string[] array1, string[] array2)
 {
@@ -14,4 +15,12 @@ void SearchArray(string[] array1, string[] array2)
         count++;
         }
     }
+}
+
+void PrintArray(string[] array){
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
